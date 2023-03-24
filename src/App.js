01e3f62c1cf83b4540019ba329f0bf0f70a4b566
kaimanwong <kai.wong@campus.lmu.de>
2023-03-24 18:06:39 +0800

    updated the App.js with the Chat  ui kit in react

--- src/App.js
+++ src/App.js
@@ -1,176 +1,120 @@
 import './App.css';
 import React, { useState } from 'react';
 import axios from "axios";
-import openai from 'openai';
-
-
-import ChatBot from 'react-simple-chatbot';
-import { Segment } from 'semantic-ui-react';
-import { ThemeProvider } from "styled-components";
-
-
-
-
-
-
-const step = [
-  {
-    id: '1',
-    message: 'Hi, I can show you some pictures. What type of picture do you want to see?',
-    trigger: 'type',
-  },
-  {
-    id: 'type',
-    options: [
-      { value: 'nature', label: 'Nature', trigger: 'nature' },
-      { value: 'animals', label: 'Animals', trigger: 'animals' },
-    ],
-  },
-  {
-    id: 'nature',
-    message: 'Here is a picture of nature:',
-    trigger: 'nature_image',
-  },
-  {
-    id: 'nature_image',
-    component: (
-      <img
-        src="https://picsum.photos/200/300"
-        width={200}
-        height={300}
-      />
-    ),
-    asMessage: true,
-    trigger: 'more_pictures',
-  },
-  {
-    id: 'animals',
-    message: 'Here is a picture of animals:',
-    trigger: 'animals_image',
-  },
-  {
-    id: 'animals_image',
-    component: (
-      <img
-        src="https://placeimg.com/200/300/animals"
-        width={200}
-        height={300}
-      />
-    ),
-    asMessage: true,
-    trigger: 'more_pictures',
-  },
-  {
-    id: 'more_pictures',
-    message: 'Do you want to see more pictures?',
-    trigger: 'more_pictures_options',
-  },
-  {
-    id: 'more_pictures_options',
-    options: [
-      { value: 'yes', label: 'Yes', trigger: 'type' },
-      { value: 'no', label: 'No', end: true },
-    ],
-  },
-];
-
-
-// Creating our own theme
-const theme = {
-  background: '#C9FF8F',
-  headerBgColor: '#197B22',
-  headerFontSize: '20px',
-  botBubbleColor: '#0F3789',
-  headerFontColor: 'white',
-  botFontColor: 'white',
-  userBubbleColor: '#FF5733',
-  userFontColor: 'white',
-};
-
-
-// Set some properties of the bot
-const config = {
-  botAvatar: "img.png",
-  floating: true,
-};
-
-
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 
 
 const App = () => {
-  //new ChatGPT API
-  // const apiKey = "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";
-  // const data = {
-  //   model: 'text-davinci-003',
-  //   prompt: 'Say this is a test',
-  //   temperature: 0,
-  //   max_tokens: 1000
-  // };
-  // const headers = {
-  //   'Content-Type': 'application/json',
-  //   'Authorization': `Bearer ${apiKey}`
-  // };
-  // const url = 'https://api.openai.com/v1/completions';
-
-
-
  
+  const [count, setCount] = useState(0);
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT! Please ask me anything.",
+      sender: "ChatGPT",
+      direction: "ingoing",
 
-  
-
-  const [prompt, setPrompt] = useState('Say this is a test');
-  const [message, setMessage] = useState('');
-
+    },
  
+  ]);
 
-   const handleSubmit = async (event) => {
-    event.preventDefault();
-    const apiKey = "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";
-    const data = {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: 'user', content: prompt }],
-    };
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`
-    };
-    const url = 'https://api.openai.com/v1/chat/completions';
 
-    try {
-      const completion = await axios.post(url, data, { headers });
-      console.log(completion.data);
-      setMessage(completion.data.choices[0].message.content);
-    } catch (error) {
-      console.error(error);
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing",
     }
-  };
+
+    // new array of messages
+    const newMessages = [...messages, newMessage]; // all the old messages, + the new messages 
+
+    //update our messages state
+    setMessages(newMessages);
+
+    //set a typing indicator (chatgpt is typing...)
+    setTyping(true);
+    //process message to chatGPT (send it over and see reponse)
+
+    await processMessageToChatGPT(newMessages);
+
+  }
+
+  async function processMessageToChatGPT(chatMessages) {
+    //chatMessages { sender: "user" or "ChatGPT", message: "The message content here"}
+    //apiMessages {role: "user" or "assistant", content: "The message content here"}
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant"
+      } else {
+        role = "user"
+      }
+      return { role: role, content: messageObject.message }
+    });
+
+    //role: "user" -> a message from the user, "assistant -> a response from chatGPT"
+    // "system" -> generally one initial message defining HOW we want chatgpt to talk
+
+    const systemMessage = {
+      role: "system",
+      content: "Explain all concepts like I am a narrator for a digital game."
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages // [message1, message2, message3]
+      ]
+    }
+
+    const apiKey = "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";
+
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data.choices[0].message.content);
+      setMessages(
+        [...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT"
+        }]
+      );
+      setTyping(false);
+    })
+  }
+
 
 
   return (
     <div>
-      <ChatBot
-        bubbleOptionStyle={{ backgroundColor: "white", color: "black" }}
-        steps={step}
-      />
 
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Prompt:
-            <input type="text" value={prompt} onChange={(event) => setPrompt(event.target.value)} />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-        {message && (
-          <div>
-            <h2>Response:</h2>
-            <p>{message}</p>
-          </div>
-        )}
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+
+        <MainContainer>
+          <ChatContainer>
+            <MessageList typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+
       </div>
-
-
 
     </div>
   );
