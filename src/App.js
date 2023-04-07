a566548f79cf946fb18aa473d72856fb73090370
latishab <65409598+latishab@users.noreply.github.com>
2023-04-07 22:51:24 +0800

    Added new features and bug fixes
    - separated messages from the clients-side
    - added typing indicator
    - removed unnecessary codes

--- src/App.js
+++ src/App.js
@@ -10,52 +10,39 @@ import audio02 from './audio02.mp3';
 
 
 const App = () => {
-
-
-
+  // Constants
+  const [userId, setUserId] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [response, setResponse] = useState('');
   const [typing, setTyping] = useState(false);
 
-  //Added the API constants
-  const [userId, setUserId] = useState('');
-  const [message, setmessage] = useState('');
-  const [response, setResponse] = useState('');
-
-
-
-  //Code for handling submit to API
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ user_id: userId, message: message }),
-    };
-
-    const apiResponse = await fetch('https://ryno-v2-cedo4cgxka-de.a.run.app/chat', requestOptions);
-    const data = await apiResponse.json();
-    setResponse(data.response);
-
-
-    const newMessage = {
-      message: data.response,
-      sender: "user",
-      direction: "ingoing",
+  // A function to split sentences to shorter messages
+  function splitSentences(responseText) {
+    const parts = responseText.split(/(\[.*?\])/g);
+    const messages = [];
+    let currentMessage = "";
+  
+    for (const part of parts) {
+      if (part.startsWith("[")) {
+        if (currentMessage.trim()) {
+          messages.push(currentMessage.trim());
+          currentMessage = "";
+        }
+        currentMessage += part;
+      } else {
+        currentMessage += part;
+      }
     }
+  
+    if (currentMessage.trim()) {
+      messages.push(currentMessage.trim());
+    }
+  
+    return messages;
+  }
 
-    // new array of messages
-    const newMessages = [...messages, newMessage]; // all the old messages, + the new messages 
-
-    //update our messages state
-    setMessages(newMessages);
-
-
-  };
-
-
-
-
-
+  // A function to handle send
   const handleSend = async (message) => {
     const newMessage = {
       message: message,
@@ -66,156 +53,52 @@ const App = () => {
     // new array of messages
     const newMessages = [...messages, newMessage]; // all the old messages, + the new messages 
 
-    //update our messages state
+    // Update our messages state
     setMessages(newMessages);
 
-    //set a typing indicator (chatgpt is typing...)
+    // Set a typing indicator (Ryno is typing...)
     setTyping(true);
-    //process message to chatGPT (send it over and see reponse)
 
-    await processMessageToChatGPT(newMessages);
+    // Send the message to your API
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ user_id: userId, message: message }),
+    };
 
-  }
+    const apiResponse = await fetch('https://ryno-v2-cedo4cgxka-de.a.run.app/chat', requestOptions);
+    const data = await apiResponse.json();
+    setResponse(data.response);
 
-  async function processMessageToChatGPT(chatMessages) {
-    //chatMessages { sender: "user" or "ChatGPT", message: "The message content here"}
-    //apiMessages {role: "user" or "assistant", content: "The message content here"}
+    // Display the response from the API
+    const separatedMessages = splitSentences(data.response);
 
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "ChatGPT") {
-        role = "assistant"
-      } else {
-        role = "user"
+    const sendSeparatedMessages = async () => {
+      for (const message of separatedMessages) {
+        setTyping(true);
+
+        // Show Ryno typing indicator between messages
+        await new Promise((resolve) => setTimeout(resolve, 1000)); 
+
+        const newMessageWithChatGPT = {
+          message: message,
+          sender: "ChatGPT",
+          direction: "ingoing",
+        };
+
+        setMessages((prevMessages) => [...prevMessages, newMessageWithChatGPT]);
+        setTyping(false);
       }
-      return { role: role, content: messageObject.message }
-    });
+    };
 
-    //role: "user" -> a message from the user, "assistant -> a response from chatGPT"
-    // "system" -> generally one initial message defining HOW we want chatgpt to talk
+    await sendSeparatedMessages();
 
-    const systemMessage = {
-      role: "system",
-      content: "Explain all concepts like I am a narrator for a digital game."
-    }
+    // hide the typing indicator after receiving the response
+    setTyping(false);
 
-    const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        systemMessage,
-        ...apiMessages // [message1, message2, message3]
-      ]
-    }
-
-    const apiKey = "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";
-
-
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        'Authorization': `Bearer ${apiKey}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      console.log(data.choices[0].message.content);
-      setMessages(
-        [...chatMessages, {
-          message: data.choices[0].message.content,
-          sender: "ChatGPT"
-        }]
-      );
-      setTyping(false);
-    })
-  }
-
-
-
-  let audio01 = new Audio("/audio01.mp3")
-  let audio02 = new Audio("/audio02.mp3")
-
-  const play_01 = () => {
-    audio01.play()
-  }
-
-  const play_02 = () => {
-    audio02.play()
-  }
-
-
-
-  const [messages, setMessages] = useState([
-    {
-      message: "Hello, I am ChatGPT! Please ask me anything.  ",
-      sender: "ChatGPT",
-      direction: "ingoing",
-    },
-    {
-      message: "This is a testing message👌.",
-      sender: "ChatGPT",
-      direction: "ingoing",
-    },
-    {
-      message: "Where can I find the Peak Source?",
-      sender: "user124",
-      direction: "outgoing",
-    },
-    {
-      message: "Ah, the Peak Source! It's one of the most enigmatic and fascinating structures in our world. [Ryno's eyes light up with excitement] To find it, we must venture to the edge of the vast Urbis Desert, where the remnants of a once-thriving metropolis stand. The journey is challenging but well worth the effort. Before we set off, would you like to know more about the history of the Peak Source and its significance to our world, especially in relation to the climate crises that have shaped our lives? <audio controls autoplay muted><source src='audio01.mp3' type='audio/mpeg'></audio>", direction: "ingoing",
-    },
-    {
-      message: " <img width='250' height='250' src='/static/media/image01.a5ba873debed7aa334de.jpeg' />   ",
-      sender: "ChatGPT",
-      direction: "ingoing",
-    },
-    {
-      message: "What is Urbis Desert? How can I get to there?",
-      sender: "user124",
-      direction: "outgoing",
-    },
-    {
-      message: "Ah, the Urbis Desert. [Ryno gazes into the distance, reminiscing] It was once a bustling urban center, full of life and technological advancements. However, due to the climate crises that ravaged our world, it transformed into a vast, barren wasteland. The desertification was a result of extreme temperatures, water scarcity, and the collapse of our once-sustainable infrastructure. To get there, we have a few options. We could travel by hovercraft, which is the fastest and most convenient way, but it requires access to a functional vehicle. Alternatively, we could embark on a thrilling journey on the back of a Sandstrider, a magnificent creature adapted to the harsh desert conditions. The journey would be slower, but it would allow us to witness the remnants of the world that once was, and the resilience of life that continues to persist despite the harsh climate. Which mode of transportation do you prefer? And do you have any questions about the Urbis Desert or the journey ahead?",
-      sender: "ChatGPT",
-      direction: "ingoing",
-    },
-    {
-      message: " <img width='250' height='250' src='/static/media/image02.95e4945ef4f26e905f0a.jpeg' />   ",
-      sender: "ChatGPT",
-      direction: "ingoing",
-    },
-    {
-      message: "What is hovercraft?",
-      sender: "user124",
-      direction: "outgoing",
-    },
-    // {
-    //   message: "Response: Today, my meal consisted of nutrient-rich algae cakes and a revitalizing beverage made from purified water and electrolytes. [Ryno rubs his stomach in satisfaction] The algae cakes are a sustainable food source that has become essential in our world, as traditional agriculture has been heavily impacted by the climate crises. By cultivating algae, we can produce food with minimal water usage and a smaller environmental footprint. How about you? Have you had a chance to try some of the unique cuisine our world has to offer? If not, I'd be happy to introduce you to some interesting dishes that have emerged as a result of our changing climate and the need for more sustainable food sources.",
-    //   sender: "ChatGPT",
-    //   direction: "ingoing",
-    // },
-  ]);
-
-
-  // const msg_style = {
-  //   fontFamily: "monospace",
-  //   background: "rgb(110, 72, 170)",
-  //   color: "rgb(255, 255, 255)"
-  // }
-
-  // const Msg = (model) => {
-
-  //   console.log(model)
-  //   return (
-  //     <div style = {msg_style}>
-  //       Hello + {model}
-  //     </div>
-  //   )
-  // }
-
-
-
+    // Clear the input field
+    setMessage("");
+  };
 
   return (
     <div>
@@ -223,36 +106,19 @@ const App = () => {
       <div style={{ position: "relative", height: "700px", width: "500px" }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}>
+            <MessageList typingIndicator={typing ? <TypingIndicator content="Ryno is typing..." /> : null}>
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />
-                // return <Msg key={i} model={message} />
 
               })}
             </MessageList>
-            <MessageInput placeholder='Type message here' onSend={console.log("onsend clicked")} />
+            <MessageInput placeholder='Type a message here...' onSend={handleSend} />
           </ChatContainer>
         </MainContainer>
       </div>
-      {/* 
-      <button style={{
-        position:'absolute',
-        zIndex: 1,
-        top: 645,
-        lef: 600,
-        height: 37,
-        width: 37,
-        borderRadius: 10,
-        backgroundColor: '#add8e6',
-        borderColor: '#add8e6',
-        marginLeft: 457,
-        marginRight: 50,
-        marginTop: 20
-      }}> > </button> */}
-
 
       <div>
-        <form onSubmit={handleSubmit}> <br/> <br/> <br /><br /><br /><br /><br /><br /><br /><br /> <br /><br /><br /><br /><br /><br /><br /><br />
+        <form> <br></br>
           <label>
             User ID:
             <input  
@@ -275,7 +141,7 @@ const App = () => {
             marginLeft: 45,
             marginRight: 50,
             marginTop: 20
-          }}  type="text" value={message} onChange={(e) => setmessage(e.target.value)} />
+          }}  type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
           </label>
          
           <input style={{
@@ -291,7 +157,13 @@ const App = () => {
             marginLeft: 457,
             marginRight: 50,
             marginTop: 20
-          }} type="submit" value=">" />
+          }} 
+          type="submit" 
+          value=">"
+          onClick={(e) => {
+            e.preventDefault();
+            handleSend(message);
+          }} />
         </form>
         {/* <p>Response: {response}</p> */}
 
