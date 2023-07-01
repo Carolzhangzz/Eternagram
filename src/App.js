fd9893fdd5cb50a9d62d5e6d907acd883461f06d
Carol_Zhang <100847020+capricekiwi@users.noreply.github.com>
2023-07-01 18:13:56 +0800

    Avoid UI Swipes on Mobile

--- src/App.js
+++ src/App.js
@@ -160,7 +160,7 @@ const App = () => {
           </div>
         </div>
       </div>
-      <div style={{ width: "100%", height: "100%" }}>
+      <div style={{ width: "100%", height: "0%"，flex :'1' }}>
         <MainContainer>
           <ChatContainer>
             <MessageList
